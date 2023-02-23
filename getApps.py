201a2a6780dd26faff8977c6e5777bbd8fe92600
fhandrick <handrick@gmail.com>
2023-02-23 14:05:55 +0100

    update for ISSTA

---
+++ getApps.py
@@ -0,0 +1,25 @@
+#To get download key acess androzoo web site
+#https://androzoo.uni.lu/ and ask for a key
+
+
+key="insert your key acess androzoo here" 
+
+import os
+import csv
+
+count = 0
+
+with open('appsHash.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        if row[0] != "Benign":
+            fileName = None
+            fileName="benign-"+"app-"+str(count+1)+"-"+row[0]+".apk"
+            print(fileName)
+            os.system("curl -G -d apikey="+key+" -d sha256="+row[0]+" https://androzoo.uni.lu/api/download -o "+fileName)
+        if row[1] != "Malicious":
+            fileName = None
+            fileName="malicious-"+"app-"+str(count+1)+"-"+row[1]+".apk"
+            print(fileName)
+            os.system("curl -G -d apikey="+key+" -d sha256="+row[1]+" https://androzoo.uni.lu/api/download -o "+fileName)
+        count=count+1
